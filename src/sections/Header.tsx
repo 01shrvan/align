c1691a7330c5b14f59e0b6d3ade173043a6990bf
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2024-12-22 19:03:30 +0530

    initial commit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,34 +1,25 @@
+import Link from "next/link";
 import ArrowRight from "@/assets/arrow-right.svg";
-import Logo from "@/assets/logosaas.png";
-import Image from "next/image";
-import MenuIcon from "@/assets/menu.svg";
+import ShimmerButton from "@/components/ui/shimmer-button";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 backdrop-blur-sm z-20">
+    <header className="z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">
-          Streamline your workflow and boost your productivity
-        </p>
-        <div className="inline-flex gap-1 items-center">
-          <p>Get started for free</p>
-          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        <p className="text-white/60 hidden md:block">Forge meaningful connections with AI-powered matchmaking and engaging microblogs.</p>
+        <div className="inline-flex gap-1 items-center group cursor-pointer transition-colors duration-200 ease-in-out hover:text-white/80">
+          <p className="group-hover:underline">Join now</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center transition-transform duration-200 ease-in-out group-hover:translate-x-1" />
         </div>
       </div>
-      <div className="py-5">
-        <div className="container">
+      <div className="py-5 flex justify-center">
+        <div className="w-[95%] max-w-6xl rounded-xl bg-gray-100/90 shadow-md p-2">
           <div className="flex items-center justify-between">
-            <Image src={Logo} alt="Saas Logo" height={40} width={40} />
-            <MenuIcon className="h-5 w-5 md:hidden" />
-            <nav className="hidden md:flex gap-6 text-black/60 items-center">
-              <a href="#">About</a>
-              <a href="#">Features</a>
-              <a href="#">Customers</a>
-              <a href="#">Updates</a>
-              <a href="#">Help</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
-                Get for free
-              </button>
+            <h1 className="font-semibold text-2xl text-gray-800">αʟιɢи</h1>
+            <nav className="flex items-center">
+              <Link href="#">
+                <ShimmerButton>Get Started</ShimmerButton>
+              </Link>
             </nav>
           </div>
         </div>
