bf8ae2d17b751342e54769e19d37e5bbe23015fe
shrvan <154585738+01shrvan@users.noreply.github.com>
2025-04-02 20:01:12 +0530

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className="z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">Forge meaningful connections with AI-powered matchmaking and engaging microblogs.</p>
+        <p className="text-white/60 hidden md:block">Forge meaningful connections with AI-powered matchmaking and engaging microblogs</p>
         <div className="inline-flex gap-1 items-center group cursor-pointer transition-colors duration-200 ease-in-out hover:text-white/80">
           <p className="group-hover:underline">Join now</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center transition-transform duration-200 ease-in-out group-hover:translate-x-1" />
